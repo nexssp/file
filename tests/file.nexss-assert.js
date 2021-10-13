38d6192378cb8a55a09dd508e1d5f3dd413f50c4
Marcin Polak <mapoart@gmail.com>
2021-10-13 09:07:06 +0200

    Fix 'which is not a function' on linux machine

--- tests/file.nexss-assert.js
+++ tests/file.nexss-assert.js
@@ -2,14 +2,17 @@ const assert = require('assert')
 const { createNewTestFolder } = require('@nexssp/test')
 const tf = createNewTestFolder()
 
+const packageFolder = process.cwd()
+const binFileCommand = `node ${packageFolder}/bin/nexssp-file`
+
 console.log('Changing folder: ', tf)
 process.chdir(tf)
 
-require('child_process').execSync('nexssp-file add myfile.js --template=default', {
+require('child_process').execSync(`${binFileCommand} add myfile.js --template=default`, {
   stdio: 'inherit',
 })
 
-require('child_process').execSync('nexssp-file add myfile.js --template=default -f', {
+require('child_process').execSync(`${binFileCommand} add myfile.js --template=default -f`, {
   stdio: 'inherit',
 })
 require('child_process').execSync('dir', { stdio: 'inherit' })
